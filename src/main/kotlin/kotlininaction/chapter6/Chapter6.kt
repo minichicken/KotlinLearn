a9a192cfbe7ebbaa93cd5b0864950dd9e1419065
Jaeho Lee <mini.chicken.dev@gmail.com>
2018-02-15 17:58:19 +0900

    update at 2018/02/15

--- src/main/kotlin/kotlininaction/chapter6/Chapter6.kt
+++ src/main/kotlin/kotlininaction/chapter6/Chapter6.kt
@@ -1,5 +1,6 @@
 package kotlininaction.chapter6
 
+import java.io.File
 import java.time.LocalDate
 
 operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
@@ -14,8 +15,25 @@ operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
             }
         }
 
+data class Point(val x: Int, val y: Int)
+
+operator fun Point.minus(other: Point): Point{
+    return Point(this.x + other.y, this.y + other.x)
+}
+
+const val path = "/opt"
+val something: File by lazy {
+    File(path)
+}
+
 fun main(args: Array<String>) {
     val newYear = LocalDate.ofYearDay(2017, 1)
     val daysOff = newYear.minusDays(1)..newYear
-    for (dayOff in daysOff) { println(dayOff) }
+    for (dayOff in daysOff) {
+        println(dayOff)
+    }
+    val x = Point(2 ,4)
+    val y = Point(3,1)
+    println(x - y)
+    something.absoluteFile
 }
